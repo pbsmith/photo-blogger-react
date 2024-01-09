1f5f93e1b05f103904c6b3fbac3eda6830a2a3ef
Pendle <pbsmith@syr.edu>
2024-01-09 17:04:57 -0500

    Gallery header

--- blog/src/components/Gallery.js
+++ blog/src/components/Gallery.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import PhotoAlbum from "react-photo-album";
+import './Gallery.css'
 
 /**Looking to present entire photo album? Or create multiple galleries? */
 
 const photos = [
     { src: "true-placeholder.jpeg", width: 800, height: 600 },
     { src: "true-placeholder.jpeg", width: 1600, height: 900 },
+    { src: "true-placeholder.jpeg", width: 1600, height: 900 },
+    { src: "true-placeholder.jpeg", width: 800, height: 600 },
+    { src: "true-placeholder.jpeg", width: 800, height: 600 },
+    { src: "true-placeholder.jpeg", width: 1600, height: 900 },
+    { src: "true-placeholder.jpeg", width: 1600, height: 900 },
     { src: "true-placeholder.jpeg", width: 800, height: 600 },
     { src: "true-placeholder.jpeg", width: 800, height: 600 },
     { src: "true-placeholder.jpeg", width: 1600, height: 900 },
-    { src: "true-placeholder.jpeg", width: 1600, height: 900 },
     { src: "true-placeholder.jpeg", width: 800, height: 600 },
-    { src: "true-placeholder.jpeg", width: 1600, height: 900 },
 ];
 
 function Gallery() {
     return (
         <>
-            <PhotoAlbum layout="rows" photos={photos} />
+            <div className='photo-album-container'>
+                <PhotoAlbum layout="rows" photos={photos} />
+            </div>
         </>
     )
 }
--- blog/src/components/pages/GalleryPage.js
+++ blog/src/components/pages/GalleryPage.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Gallery from '../Gallery.js'
 import './GalleryPage.css'
 import Background from '../Background.js'
-
+import '../../App.js'
 function GalleryPage() {
     return (
     <>
-    <Background/>
+    <div>
+                        <Background />
+        <div className='gallery-header'>
+                <p className='header'>Choose an album from the gallery below...</p>
+        </div>
         <div className='gallery-container'>
             <Gallery/>
+        </div>
         </div>
     </>
   )
